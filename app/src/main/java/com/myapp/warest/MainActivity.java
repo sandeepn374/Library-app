40ea88082d5084d275c9bd6a7f973a5ec9931d1d
sandeepn374 <sandeepn374@gmail.com>
2017-11-25 14:58:18 +0530

    adding main activity

--- app/src/main/java/com/myapp/warest/MainActivity.java
+++ app/src/main/java/com/myapp/warest/MainActivity.java
@@ -1,5 +1,17 @@
 package com.myapp.warest;
 
-public class MainActivity
-{
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
+
+
+
+
