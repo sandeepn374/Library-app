18768faf7499d6bafd5af074bfb2e8538f5e848e
kshravi86 <18forever>
2017-11-26 22:03:05 +0530

    everything working fineas now

--- app/src/main/java/com/myapp/warest/StudentActivity.java
+++ app/src/main/java/com/myapp/warest/StudentActivity.java
@@ -1,16 +1,12 @@
 package com.myapp.warest;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
 public class StudentActivity extends AppCompatActivity
@@ -19,7 +15,7 @@ public class StudentActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main1);
+        setContentView(R.layout.student);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
