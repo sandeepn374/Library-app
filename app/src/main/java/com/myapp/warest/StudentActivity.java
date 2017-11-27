52a575f7fce0eee0bf20d0d8943571cf05a79ca5
sandeepn374 <sandeepn374@gmail.com>
2017-11-27 13:45:06 +0530

    adding student activity

--- app/src/main/java/com/myapp/warest/StudentActivity.java
+++ app/src/main/java/com/myapp/warest/StudentActivity.java
@@ -1,73 +1,144 @@
 package com.myapp.warest;
 
-import android.content.Intent;
+import android.annotation.TargetApi;
+import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.FrameLayout;
 
-public class StudentActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+
+
+public class StudentActivity extends AppCompatActivity {
+    ActionBarDrawerToggle drawerToggle;
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+
+    FragmentManager fragmentManager;
+    NavigationView navigationView;
+    FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.student);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_main_student);
 
+        fragmentManager = getSupportFragmentManager();
 
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        setupView();
+        if (savedInstanceState == null) showHome();
     }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
+    private void setupView() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        frameLayout = (FrameLayout) findViewById(R.id.content_frame);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close);
+        drawerLayout.addDrawerListener(drawerToggle);
+
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+				@Override
+				public boolean onNavigationItemSelected(MenuItem menuItem) {
+					selectDrawerItem(menuItem);
+					return true;
+				}
+			});
+    }
+
+    private void showHome() {
+        selectDrawerItem(navigationView.getMenu().getItem(0));
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    private void selectDrawerItem(MenuItem menuItem) {
+        boolean specialToolbarBehaviour = false;
+        Class fragmentClass;
+
+        switch (menuItem.getItemId()) {
+            case R.id.aboutus:
+                fragmentClass = HomeFragment.class;
+                break;
+
+            default:
+                fragmentClass = HomeFragment.class;
+                break;
+        }
+
+        try {
+            Fragment fragment = (Fragment) fragmentClass.newInstance();
+            fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        setToolbarElevation(specialToolbarBehaviour);
+        menuItem.setChecked(true);
+        setTitle(menuItem.getTitle());
+        drawerLayout.closeDrawers();
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void setToolbarElevation(boolean specialToolbarBehaviour) {
+        if (specialToolbarBehaviour) {
+            toolbar.setElevation(0.0f);
+            frameLayout.setElevation(getResources().getDimension(R.dimen.elevation_toolbar));
         } else {
-            super.onBackPressed();
+            toolbar.setElevation(getResources().getDimension(R.dimen.elevation_toolbar));
+            frameLayout.setElevation(0.0f);
         }
     }
 
+    public void showSnackbarMessage(View v) {/*
+		 EditText et_snackbar = (EditText) findViewById(R.id.et_snackbar);
+		 TextInputLayout textInputLayout = (TextInputLayout) findViewById(R.id.textInputLayout);
+		 View view = findViewById(R.id.coordinator_layout);
+		 if (et_snackbar.getText().toString().isEmpty()) {
+		 textInputLayout.setError(getString(R.string.alert_text));
+		 } else {
+		 textInputLayout.setErrorEnabled(false);
+		 et_snackbar.onEditorAction(EditorInfo.IME_ACTION_DONE);
+		 Snackbar.make(view, et_snackbar.getText().toString(), Snackbar.LENGTH_LONG)
+		 .setAction(getResources().getString(android.R.string.ok), new View.OnClickListener() {
+		 @Override
+		 public void onClick(View v) {
+		 // Do nothing
+		 }
+		 })
+		 .show();
+		 }*/
+    }
 
-
-
-
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return drawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
 
-       /* if (id == R.id.logout) {
-            Intent i = new Intent(this, StartScreenActivity.class);
-            startActivity(i);
-            // Handle the camera action
-        } */
-        if (id == R.id.contactus) {
-            Intent i = new Intent(this, ContactusActivity.class);
-            startActivity(i);
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        drawerToggle.syncState();
+        super.onPostCreate(savedInstanceState);
+    }
 
-        }/* else if (id == R.id.nav_slideshow) {
-
-        }*/
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
     }
 }
-
