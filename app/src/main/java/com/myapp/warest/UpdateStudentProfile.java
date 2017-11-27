3b2aae40a83df153c2bd7fb75ebab434f84c00b0
sandeep374 <sandeepn374@gmail.com>
2017-11-27 21:57:50 +0530

    Committing profile picture update

--- app/src/main/java/com/myapp/warest/UpdateStudentProfile.java
+++ app/src/main/java/com/myapp/warest/UpdateStudentProfile.java
@@ -29,6 +29,11 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import android.app.*;
+import com.google.firebase.storage.*;
+import android.net.*;
+import com.google.android.gms.tasks.*;
+import android.support.annotation.*;
 
 /**
  * Created by kshravi on 26/11/2017 AD.
@@ -39,12 +44,20 @@ public class UpdateStudentProfile extends AppCompatActivity {
     EditText name,age,course,college,branch,country,university,state,city,reTrain;
      FirebaseAuth auth;
     Spinner gender;
-    Button update,view;
+    Button update,view,upload;
+
+	ProgressDialog pd;
+
+    //creating reference to firebase storage
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+    StorageReference storageRef = storage.getReferenceFromUrl("gs://warest-77e4b.appspot.com/");
+	
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.updatestudentprofile);
+		
         name=(EditText)findViewById(R.id.fullname);
         age=(EditText)findViewById(R.id.age);
         course=(EditText)findViewById(R.id.ongoingcourse);
@@ -62,6 +75,20 @@ public class UpdateStudentProfile extends AppCompatActivity {
 
         update=(Button)findViewById(R.id.update);
         view=(Button) findViewById(R.id.viewprofile);
+		upload=(Button)findViewById(R.id.upload);
+	
+		pd = new ProgressDialog(this);
+        pd.setMessage("Uploading....");
+	
+		upload.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					Intent myIntent = new Intent(Intent.ACTION_GET_CONTENT);
+					myIntent.setType("image/*");
+					startActivityForResult(Intent.createChooser(myIntent,"Select File:-"),101);
+				}
+			});
+	
 
         view.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -107,42 +134,57 @@ public class UpdateStudentProfile extends AppCompatActivity {
                                 child.getRef().child("city").setValue(city.getText().toString());
                                 child.getRef().child("requiredTrain").setValue(reTrain.getText().toString());
                                 Toastmsg(UpdateStudentProfile.this,"Profile Updated ");
-
-
-
-
-
-
-
-
                             }
-
-
                         }
-
-
                     }
-
                     @Override
                     public void onCancelled(DatabaseError databaseError) {
-
                     }
                 });
-                ;
-
-
             }
-
-
-
-
         });
-
-
-
-
-
             }
+		
+		
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		if(resultCode==RESULT_CANCELED)
+		{
+			// action cancelled
+		}
+		if(resultCode==RESULT_OK)
+		{
+			
+			// Create a storage reference 
+			Uri uri = data.getData();
+			if(uri != null) {
+				pd.show();
+			}
+			StorageReference riversRef = storageRef.child(auth.getCurrentUser().getEmail());
+			UploadTask uploadTask = riversRef.putFile(uri);
+
+			// Register observers to listen for when the download is done or if it fails
+			uploadTask.addOnFailureListener(new OnFailureListener() {
+					@Override
+					public void onFailure(@NonNull Exception exception) {
+						// Handle unsuccessful uploads
+
+						Toast.makeText(UpdateStudentProfile.this, "Upload Failed", Toast.LENGTH_SHORT).show();
+					}
+				}).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+					@Override
+					public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+						// taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+
+						Toast.makeText(UpdateStudentProfile.this, "Upload Success", Toast.LENGTH_SHORT).show();
+					}
+				});
+			pd.dismiss();
+		}
+	}	
+		
+			
     private void Toastmsg(UpdateStudentProfile collectionActivity, String p1)
     {
 
