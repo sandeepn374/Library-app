29671c1427309b79f23fe2792f0feabd2549a75e
kshravi86 <18forever>
2017-12-06 15:39:47 +0530

    on back pressed implemented

--- app/src/main/java/com/myapp/warest/FacultyActivity.java
+++ app/src/main/java/com/myapp/warest/FacultyActivity.java
@@ -233,7 +233,13 @@ public class FacultyActivity extends AppCompatActivity
         drawerToggle.onConfigurationChanged(newConfig);
     }
 
+    public void onBackPressed(){
+        Intent a = new Intent(Intent.ACTION_MAIN);
+        a.addCategory(Intent.CATEGORY_HOME);
+        a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(a);
 
+    }
 
 
 }
