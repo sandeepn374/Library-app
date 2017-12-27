fe5a727aa3e76462b82098676820aca062f6bb1f
kshravi86 <18forever>
2017-12-27 16:14:07 +0530

    more button added student profile view

---
+++ app/src/main/java/com/myapp/warest/StudentProfileView.java
@@ -0,0 +1,17 @@
+package com.myapp.warest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kshravi on 27/12/2017 AD.
+ */
+
+public class StudentProfileView extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.studentprofileview);
+    }
+}
--- app/src/main/java/com/myapp/warest/StudentViewActivity.java
+++ app/src/main/java/com/myapp/warest/StudentViewActivity.java
@@ -1,35 +1,24 @@
 package com.myapp.warest;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
-import android.widget.SearchView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class StudentViewActivity extends AppCompatActivity {
 
@@ -85,12 +74,33 @@ public class StudentViewActivity extends AppCompatActivity {
                             tr2.addView(tv2);
                             tv2.setTextColor(Color.WHITE);
                             tv2.setTextSize(getResources().getDimension(R.dimen.text_size));
+                            TableRow tr4 = new TableRow(StudentViewActivity.this);
+
+                            Button paid = new Button(StudentViewActivity.this);
+                            paid.setText("More");
+                            paid.setTextColor(Color.BLACK);
+                            paid.setGravity(Gravity.LEFT);
+                            int height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 40, getResources().getDisplayMetrics());
+                            int width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 30, getResources().getDisplayMetrics());
+                            paid.setLayoutParams(new TableRow.LayoutParams(width, height));
+                            // paid.setWidth(5);
+
+                            paid.setOnClickListener(new View.OnClickListener() {
+                                                        @Override
+                                                        public void onClick(View view) {
+                                                            startActivity(new Intent(StudentViewActivity.this, StudentProfileView.class));
+                                                        }
+                                                    });
+                            tr4.setLayoutParams(params);
+                            tr4.addView(paid);
 
                             layoutINNER.addView(tr2);
+                            layoutINNER.addView(tr4);
                             View line = new View(StudentViewActivity.this);
                             line.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.FILL_PARENT, 1));
                             line.setBackgroundColor(Color.rgb(51, 51, 51));
                             layoutINNER.addView(line);
+
 
 
 
