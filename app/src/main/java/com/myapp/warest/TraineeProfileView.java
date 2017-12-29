5d1eadff3392a4e5a4042571ff117de4ca666f69
kshravi86 <18forever>
2017-12-29 22:23:44 +0530

    freelancer profile view admin

--- app/src/main/java/com/myapp/warest/TraineeProfileView.java
+++ app/src/main/java/com/myapp/warest/TraineeProfileView.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 public class TraineeProfileView extends AppCompatActivity {
 
 
-    TextView name,age,designation,gender,qualification;
+    TextView name,age,designation,gender,qualification,ph,email;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,14 +21,18 @@ public class TraineeProfileView extends AppCompatActivity {
         name=(TextView)findViewById(R.id.traineename);
         age=(TextView)findViewById(R.id.traineeage);
         designation=(TextView)findViewById(R.id.designation);
+        email=(TextView)findViewById(R.id.traineeemail);
 
         gender=(TextView)findViewById(R.id.traineegender);
         qualification=(TextView)findViewById(R.id.qualification);
+        ph=(TextView)findViewById(R.id.ph);
 
         FacultyTrainee s=(FacultyTrainee) getIntent().getSerializableExtra("MyClass");
         name.setText("Name :  " +s.name);
 
         age.setText("Age   :  "+s.age);
+        ph.setText("Phone:"+s.ph);
+        email.setText("Email:" +s.email);
 
         gender.setText("Gender : "+s.gender);
 
