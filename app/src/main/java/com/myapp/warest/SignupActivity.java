6323d7f215df6a6ba5b8f28c475adabd01313593
kshravi86 <kshravi86@gmail.com>
2018-03-13 14:40:46 +0530

    signup auth working fine

--- app/src/main/java/com/myapp/warest/SignupActivity.java
+++ app/src/main/java/com/myapp/warest/SignupActivity.java
@@ -32,11 +32,11 @@ import java.util.Map;
 
 public class SignupActivity extends AppCompatActivity {
 
-    private EditText inputEmail, inputPassword,fullname,phone;
-    private Button btnSignIn, btnSignUp;
+    private EditText name, email, phone, password ,cnfrmpwd, usn ;
+    private Button btnReg;
     private ProgressBar progressBar;
     private FirebaseAuth auth;
-    private Spinner gender;
+    private Spinner gender, sem, dept;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +44,112 @@ public class SignupActivity extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
 
         auth = FirebaseAuth.getInstance();
+		
+		name = (EditText)findViewById(R.id.fullname);
+		email = (EditText)findViewById(R.id.email);
+		phone = (EditText)findViewById(R.id.phone);
+		password = (EditText)findViewById(R.id.password);
+        cnfrmpwd = (EditText)findViewById(R.id.confirmpwd);
+		usn = (EditText)findViewById(R.id.usn);
+		gender = (Spinner)findViewById(R.id.gender);
+		sem = (Spinner)findViewById(R.id.semester);
+		dept = (Spinner)findViewById(R.id.department);
+		btnReg = (Button)findViewById(R.id.register);
+		progressBar=(ProgressBar)findViewById(R.id.progressBar);
+		
+		
+		
+		
 
-        
+        btnReg.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+
+					String inputemail = email.getText().toString().trim();
+					String inputpassword = password.getText().toString().trim();
+					String inputusn = usn.getText().toString();
+					String inputname = name.getText().toString();
+					String inputphone = phone.getText().toString();
+					String inputgender= gender.getSelectedItem().toString();
+					String inputsem= sem.getSelectedItem().toString();
+					String inputdept= dept.getSelectedItem().toString();
+					
+					if (TextUtils.isEmpty(inputname)) {
+						Toast.makeText(getApplicationContext(), "Enter your name!", Toast.LENGTH_SHORT).show();
+						return;
+					}
+					
+					if (TextUtils.isEmpty(inputphone)) {
+						Toast.makeText(getApplicationContext(), "Enter phone no!", Toast.LENGTH_SHORT).show();
+						return;
+					}
+					if (TextUtils.isEmpty(inputemail)) {
+						Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+						return;
+					}
+
+					if (TextUtils.isEmpty(inputpassword)) {
+						Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+						return;
+					}
+					
+					if (TextUtils.isEmpty(inputusn)) {
+						Toast.makeText(getApplicationContext(), "Enter your USN!", Toast.LENGTH_SHORT).show();
+						return;
+					}
+
+					if (password.length() < 6) {
+						Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+						return;
+					}
+					
+					if (gender.getSelectedItem().equals("Select Your Gender")) {
+						Toast.makeText(getApplicationContext(), "Please Select your gender", Toast.LENGTH_SHORT).show();
+						return;
+					}
+					
+					if (sem.getSelectedItem().equals("Select Your Semester")) {
+						Toast.makeText(getApplicationContext(), "Please Select your semester", Toast.LENGTH_SHORT).show();
+						return;
+					}
+					
+					if (dept.getSelectedItem().equals("Select Your Department")) {
+						Toast.makeText(getApplicationContext(), "Please Select your department", Toast.LENGTH_SHORT).show();
+						return;
+					}
+
+					progressBar.setVisibility(View.VISIBLE);
+					//create user
+					auth.createUserWithEmailAndPassword(inputemail, inputpassword)
+                        .addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Toast.makeText(SignupActivity.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+                                progressBar.setVisibility(View.GONE);
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(SignupActivity.this, "Authentication failed." + task.getException(),
+												   Toast.LENGTH_SHORT).show();
+                                } else {
+
+                                  //  String inputname=name.getText().toString();
+                                    //String inputph=phone.getText().toString();
+                                  //  String inputemail=email.getText().toString();
+									//String inputusn = usn.getText().toString();
+									
+									
+									Intent intent = new Intent(SignupActivity.this, MainActivity.class);
+									startActivity(intent);
+									finish();
+		
+		
+}
+}
+
+});
+}
+});
 }
 }
