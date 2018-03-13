9a7a4d5bbe8b46d2cb6d2d0dcda5c33783961328
kshravi86 <kshravi86@gmail.com>
2018-03-13 12:39:16 +0530

    signup layout implemented fully

--- app/src/main/java/com/myapp/warest/LoginActivity.java
+++ app/src/main/java/com/myapp/warest/LoginActivity.java
@@ -28,7 +28,7 @@ public class LoginActivity extends AppCompatActivity
 	private EditText inputEmail, inputPassword;
 	private FirebaseAuth auth;
 	private ProgressBar progressBar;
-	private Button btnSignup, btnLogin, btnReset;
+	private Button btnSignup, btnLogin;
 	// private Spinner community;
 
 
