486cf93a4824c9582cc99fcbd421670ec9e6e4c8
kshravi86 <kshravi86@gmail.com>
2018-03-18 15:41:02 +0530

    book activity and layout added

---
+++ app/src/main/java/com/myapp/warest/BookActivity.java
@@ -0,0 +1,20 @@
+package com.myapp.warest;
+import android.os.*;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.*;
+import android.view.*;
+import android.content.*;
+
+public class BookActivity extends AppCompatActivity
+{
+	
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book);
+	
+	
+	
+}}
--- app/src/main/java/com/myapp/warest/MainActivity.java
+++ app/src/main/java/com/myapp/warest/MainActivity.java
@@ -2,13 +2,32 @@ package com.myapp.warest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.*;
+import android.view.*;
+import android.content.*;
 
 public class MainActivity extends AppCompatActivity {
+	
+	
+	Button stn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+		stn = (Button)findViewById(R.id.button1);
+		
+stn.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					//button functionality
+					startActivity(new Intent(MainActivity.this, BookActivity.class));
+				}
+			});
+		
+		
+		
+		
     }
 }
 
